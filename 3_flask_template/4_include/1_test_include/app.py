3d117e28d48d3176da2929199d99d30a5602afe5
chenyuchuting <757403038@qq.com>
2020-02-24 17:18:38 +0800

    include and static

---
+++ 3_flask_template/4_include/1_test_include/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+# 配置自动加载模板文件
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
