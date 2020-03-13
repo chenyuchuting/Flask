4fcf133a106747777e705e3d62d719cd02102925
chenyuchuting <757403038@qq.com>
2020-03-13 17:22:14 +0800

    设置cookie有效期

--- 6_WTForms_Cookie_Files_CRSF/5_cookie/1_test_cookie/app.py
+++ 6_WTForms_Cookie_Files_CRSF/5_cookie/1_test_cookie/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, Response
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -6,7 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     response = Response('设置cookie')
-    response.set_cookie('username', 'chen')
+
+    # expires = datetime(year=2020, month=4, day=13, hour=16, minute=52, second=20)
+    # response.set_cookie('username', 'chen', max_age=60)  # max_age是表示cookie有效的时间，以秒为单位
+    # expires设置的是到期的时间，是格林尼治时间
+    # response.set_cookie('username', 'chen',  expires=expires)
+
+    expires = datetime.now() + timedelta(days=30, hours=16)
+    response.set_cookie('username', 'chen', expires=expires)
     return response
 
 @app.route('/del/')
