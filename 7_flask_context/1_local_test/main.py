52cc2775a4e93875eac5a11a5918549b6cb0edee
chenyuchuting <757403038@qq.com>
2020-04-22 18:50:22 +0800

    上下文

--- 7_flask_context/1_local_test/main.py
+++ 7_flask_context/1_local_test/main.py
@@ -1,17 +1,19 @@
 from threading import Thread
+from werkzeug.local import Local
 
+local = Local()
 
-request = '123'
+local.request = '123'
 
 
 class MyThread(Thread):
     def run(self):
         global request
-        request = 'abc'
-        print("子线程：", request)
+        local.request = 'abc'
+        print("子线程：", local.request)
 
 
 mythread = MyThread()
 mythread.start()
 mythread.join()
-print("主线程:", request)
+print("主线程：", local.request)
