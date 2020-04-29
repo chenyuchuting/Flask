2d2daad4262e0d100ad7377296cf70a49653f57a
chenyuchuting <757403038@qq.com>
2020-04-29 19:59:32 +0800

    Hook函数更新

--- 8_flask_hook/1_test_hook/app.py
+++ 8_flask_hook/1_test_hook/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, g, request
+from flask import Flask, session, g, request, render_template, abort
 import os
 
 app = Flask(__name__)
@@ -12,13 +12,20 @@ def hello_world():
     g.username = username
     if hasattr(g, 'user'):
         print(g.user)
-    return 'Hello World!'
+    # return render_template('list.html', current_user='chain2')
+    return render_template('index.html')
 
 
 @app.route("/list/")
 def my_list():
     session['user_id'] = 1
-    return "My list!"
+    user_id = request.args.get("user_id")
+    # http://127.0.0.1:5000/list/?user_id=1
+    if user_id == '1':
+        return 'hello'
+    else:
+        abort(500)
+    return render_template('list.html')
 
 
 
@@ -32,7 +39,27 @@ def first_request():
 def before_request():
     user_id = session.get('user_id')
     if user_id:
-        g.user = 'chain'
+        g.user = 'chain1'
+
+
+@app.context_processor
+def context_processor():
+    # return {'current_user': 'chain2'}
+    if hasattr(g, 'user'):
+        return {'current_user': g.user}
+    else:
+        # return None  # TypeError:'NoneType'object is not iterable
+        return {}
+
+
+@app.errorhandler(500)
+def server_error(error):  # error不可少
+    return render_template('500.html'), 500
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
